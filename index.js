9d73afb636823fe50686c46cd778007883e7901f
Riardot <115577197+Riardot@users.noreply.github.com>
2024-08-27 22:52:20 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+function encriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+
+  if (texto.length != 0) {
+    document.getElementById("texto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto encriptado con éxito";
+    parrafo.textContent = "";
+    muñeco.src = "./img/encriptado.jpg";
+  } else {
+    muñeco.src = "./img/muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+    parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    swal("Ooops!", "Debes ingresar un texto", "warning");
+  }
+}
+
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+  let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  
+    if (texto.length != 0) {
+      document.getElementById("texto").value = textoCifrado;
+      tituloMensaje.textContent = "Texto desencriptado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "./img/desencriptado.jpg";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+      swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}
+
+function copiar() {
+
+    const mensajeEncriptado = document.getElementById('mensaje-encriptado');
+    const titulo = mensajeEncriptado.querySelector('#titulo-mensaje').textContent;
+    const parrafo = mensajeEncriptado.querySelector('#parrafo').textContent;
+    const textoParaCopiar = `${titulo}\n${parrafo}`;
+
+    const textarea = document.createElement('textarea');
+    textarea.value = textoParaCopiar;
+    document.body.appendChild(textarea);
+
+
+    textarea.select();
+    textarea.setSelectionRange(0, 99999); 
+    document.execCommand('copy');
+
+   
+    document.body.removeChild(textarea);
+
+    
+    alert('Texto copiado al portapapeles: \n' );
+}
+
